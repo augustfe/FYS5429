8c23cb6607ed6842cd17d038ff0cb6fc8ee0307c
August Femtehjell <august.femtehjell@gmail.com>
2024-02-19 17:17:58 +0100

    Added boundary constraints, working on interface

--- Project1XPINNs/src/region.py
+++ Project1XPINNs/src/region.py
@@ -10,6 +10,7 @@ class Region:
     ):
         self.constraint_file = Path(constraint_file)
         self.areas: list[Area] = []
+        self.limits = []
 
         if use_seed:
             np.random.seed(seed)
@@ -17,10 +18,37 @@ class Region:
         self.read_constraint_file()
 
     def read_constraint_file(self):
-        """Read constraints from file and generate corresponding regions."""
+        """Read constraints from file and generate corresponding regions.
+
+        File must be of the format
+        # of arguments
+            low high
+        # of areas
+            # of constraints for area
+            constraits of the form
+            C0 C1 .. Cn K
+            meaning
+            x0 * C0 + x1 * C1 + ... + xn * Cn <= K
+        # of boundaries
+            # of boundaries for region
+                x00 x10 ... xn0 x01 x11 ... xn1
+                translating to the start point
+                [x00, x10, ..., xn0]
+                and the end point
+                [x01, x11, ..., xn1]
+        """
         with open(self.constraint_file, "r") as infile:
+            # Number of arguments
+            n_args = int(infile.readline())
+            for _ in range(n_args):
+                # low, high constraint for each arg
+                lower, higher = map(float, infile.readline().split())
+                self.limits.append((lower, higher))
+
+            # Number of areas
             N = int(infile.readline())
-            for i in range(N):
+            for _ in range(N):
+                # Number of constraints for area
                 M = int(infile.readline())
                 curr_area = Area()
                 for j in range(M):
@@ -28,41 +56,79 @@ class Region:
                     curr_area.add_ineq(x_C, t_C, const)
                 self.areas.append(curr_area)
 
+            # Number of boundaries
+            N = int(infile.readline())
+            for i in range(N):
+                # Number of boundaries per region
+                m = int(infile.readline())
+                for _ in range(m):
+                    # Start and end points of boundary
+                    values = list(map(float, infile.readline().split()))
+                    start = np.asarray(values[:n_args])
+                    end = np.asarray(values[n_args:])
+                    self.areas[i].add_boundary(start, end)
+
     def add_points(self, X: np.ndarray):
         """Add array of points to the corresponding areas
 
         Args:
             X (np.ndarray): Array of all points
         """
-        for x, t in X:
+        for point in X:
             for area in self.areas:
-                if not area.is_in_area(x, t):
+                if not area.is_in_area(point):
                     continue
-                area.add_point(x, t)
+                area.add_point(point)
                 break
             else:
-                print(f"WARNING: Can't place ({x}, {t}) in an area!!")
+                print(f"WARNING: Can't place ({point}) in an area!!")
 
-    def test_points(
-        self, xlim: tuple[float] = (-1, 1), tlim: tuple[float] = (0, 1), N: int = 2000
-    ) -> np.ndarray:
+    def test_points(self, N: int = 2000) -> np.ndarray:
         """Populate region with test-points.
 
         Args:
-            xlim (tuple[float], optional): (min, max) for x values. Defaults to (-1, 1).
-            tlim (tuple[float], optional): (min, max) for t values. Defaults to (0, 1).
             N (int, optional): Number of points to generate. Defaults to 2000.
 
         Returns:
             np.ndarray: Array of generated points.
         """
-        x_vals = np.random.uniform(xlim[0], xlim[1], (N, 1))
-        t_vals = np.random.uniform(tlim[0], tlim[1], (N, 1))
-        X = np.hstack([x_vals, t_vals])
+        arrs = []
+        for lower, higher in self.limits:
+            arrs.append(np.random.uniform(lower, higher, (N, 1)))
 
+        X = np.hstack(arrs)
         self.add_points(X)
 
         return X
+
+    def test_boundary_points(self, N: int = 200) -> np.ndarray:
+        placed = 0
+        num_areas = len(self.areas)
+        new_points = []
+        while placed <= N:
+            # Choose area
+            chosen_area = self.areas[np.random.choice(num_areas)]
+            n_bound = len(chosen_area.boundary_constraints)
+            # Check area has boundary constraints
+            if n_bound == 0:
+                continue
+
+            # Choose boundary
+            chosen_bound = np.random.choice(n_bound)
+            start, end = chosen_area.boundary_constraints[chosen_bound]
+
+            # Generate new point through interpolation
+            dist = np.random.uniform(0, 1)
+            new_point = start + (end - start) * dist
+            chosen_area.bound_points.append(new_point)
+
+            new_points.append(new_point)
+            placed += 1
+
+        for area in self.areas:
+            area.bound_points = np.asarray(area.bound_points)
+
+        return np.asarray(new_points)
 
     def plot_points(self):
         """Plot the points in the corresponding areas of the region."""
@@ -71,15 +137,27 @@ class Region:
             plt.scatter(
                 area.df_points["x"], area.df_points["t"], s=2.5, label=f"Region {i+1}"
             )
+            bounds = area.bound_points
+            plt.scatter(
+                bounds[:, 0],
+                bounds[:, 1],
+                marker="x",
+                alpha=0.4,
+                label=f"Boundary {i + 1}",
+            )
         plt.legend()
         plt.show()
 
 
 class Area:
     def __init__(self):
-        self.constraints: list[tuple[float, float, float]] = []
+        self.constraints: list[list[float, float, float]] = []
         self.points: list[list[float, float]] = []
         self.df_points: pd.DataFrame = None
+        self.coeff_matrix: np.ndarray = None
+        self.const_matrix: np.ndarray = None
+        self.boundary_constraints: list[tuple[list]] = []
+        self.bound_points = []
 
     def add_ineq(self, x_C: float, t_C: float, const: float) -> None:
         """Add inequality for 2D
@@ -96,7 +174,19 @@ class Area:
         for val in vals:
             if not isinstance(val, float):
                 raise ValueError(f"{val} must be float.")
-        self.constraints.append((x_C, t_C, const))
+        self.constraints.append([x_C, t_C, const])
+
+    def add_boundary(self, start_point: np.ndarray, end_point: np.ndarray) -> None:
+        """Add pair of points defining a boundary.
+
+        e.g.:
+            [1, 1] to [1, 0]
+
+        Args:
+            start_point (np.ndarray): Start point for boundary
+            end_point (np.ndarray): End point for boundary
+        """
+        self.boundary_constraints.append((start_point, end_point))
 
     def read_ineq_from_file(self, filename: str | Path) -> None:
         """Read the constraints of the region from file.
@@ -122,7 +212,7 @@ class Area:
                 x_C, t_C, const = map(float, line.split())
                 self.add_ineq(x_C, t_C, const)
 
-    def read_ineq_from_list(self, values: list[tuple[float, float, float]]) -> None:
+    def read_ineq_from_list(self, values: list[list[float, float, float]]) -> None:
         """Add inequalities from list of values
 
         Tuples must be of the form
@@ -135,7 +225,7 @@ class Area:
             ValueError: if wrong number of arguments in a constraint
         """
         for value in values:
-            value = tuple(value)
+            value = list(value)
 
             if not len(value) == 3:
                 raise ValueError(
@@ -145,7 +235,7 @@ class Area:
             if value not in self.constraints:
                 self.constraints.append(value)
 
-    def is_in_area(self, x: float, t: float) -> bool:
+    def is_in_area(self, point: np.ndarray) -> bool:
         """Check if the point is in the region
 
         Args:
@@ -155,20 +245,21 @@ class Area:
         Returns:
             bool: Whether the point is within the given area
         """
-        for x_C, t_C, const in self.constraints:
-            if not x * x_C + t * t_C <= const:
-                return False
+        if self.coeff_matrix is None:
+            tmp = np.asarray(self.constraints)
+            self.coeff_matrix = tmp[:, :-1]
+            self.const_matrix = tmp[:, -1]
 
-        return True
+        return np.all(self.coeff_matrix @ point <= self.const_matrix)
 
-    def add_point(self, x: float, t: float) -> None:
+    def add_point(self, point: np.ndarray) -> None:
         """Add a point to the area
 
         Args:
             x (float): x value of point
             t (float): t value of point
         """
-        self.points.append([x, t])
+        self.points.append(point)
 
     def points_to_df(self) -> None:
         """Convert points from list to dataframe."""
