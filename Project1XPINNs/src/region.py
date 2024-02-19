6251f6db51431daaa34baf1ec17c2d26c8f2aa49
August Femtehjell <august.femtehjell@gmail.com>
2024-02-19 13:37:07 +0100

    Added Region interface, working example in testing/playground

--- Project1XPINNs/src/region.py
+++ Project1XPINNs/src/region.py
@@ -1,11 +1,85 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Region:
+    def __init__(
+        self, constraint_file: str | Path, seed: int = 0, use_seed: bool = True
+    ):
+        self.constraint_file = Path(constraint_file)
+        self.areas: list[Area] = []
+
+        if use_seed:
+            np.random.seed(seed)
+
+        self.read_constraint_file()
+
+    def read_constraint_file(self):
+        """Read constraints from file and generate corresponding regions."""
+        with open(self.constraint_file, "r") as infile:
+            N = int(infile.readline())
+            for i in range(N):
+                M = int(infile.readline())
+                curr_area = Area()
+                for j in range(M):
+                    x_C, t_C, const = map(float, infile.readline().split())
+                    curr_area.add_ineq(x_C, t_C, const)
+                self.areas.append(curr_area)
+
+    def add_points(self, X: np.ndarray):
+        """Add array of points to the corresponding areas
+
+        Args:
+            X (np.ndarray): Array of all points
+        """
+        for x, t in X:
+            for area in self.areas:
+                if not area.is_in_area(x, t):
+                    continue
+                area.add_point(x, t)
+                break
+            else:
+                print(f"WARNING: Can't place ({x}, {t}) in an area!!")
+
+    def test_points(
+        self, xlim: tuple[float] = (-1, 1), tlim: tuple[float] = (0, 1), N: int = 2000
+    ) -> np.ndarray:
+        """Populate region with test-points.
+
+        Args:
+            xlim (tuple[float], optional): (min, max) for x values. Defaults to (-1, 1).
+            tlim (tuple[float], optional): (min, max) for t values. Defaults to (0, 1).
+            N (int, optional): Number of points to generate. Defaults to 2000.
+
+        Returns:
+            np.ndarray: Array of generated points.
+        """
+        x_vals = np.random.uniform(xlim[0], xlim[1], (N, 1))
+        t_vals = np.random.uniform(tlim[0], tlim[1], (N, 1))
+        X = np.hstack([x_vals, t_vals])
+
+        self.add_points(X)
+
+        return X
+
+    def plot_points(self):
+        """Plot the points in the corresponding areas of the region."""
+        for i, area in enumerate(self.areas):
+            area.points_to_df()
+            plt.scatter(
+                area.df_points["x"], area.df_points["t"], s=2.5, label=f"Region {i+1}"
+            )
+        plt.legend()
+        plt.show()
 
 
 class Area:
     def __init__(self):
         self.constraints: list[tuple[float, float, float]] = []
         self.points: list[list[float, float]] = []
+        self.df_points: pd.DataFrame = None
 
     def add_ineq(self, x_C: float, t_C: float, const: float) -> None:
         """Add inequality for 2D
@@ -87,8 +161,30 @@ class Area:
 
         return True
 
-    def write_points_to_file(self, filename: str | Path):
+    def add_point(self, x: float, t: float) -> None:
+        """Add a point to the area
+
+        Args:
+            x (float): x value of point
+            t (float): t value of point
+        """
+        self.points.append([x, t])
+
+    def points_to_df(self) -> None:
+        """Convert points from list to dataframe."""
+        if self.df_points is not None:
+            print("WARNING: Overwriting previous points.")
+        self.df_points = pd.DataFrame(self.points, columns=["x", "t"], dtype=float)
+
+    def write_points_to_file(self, filename: str | Path) -> None:
+        """Write dataframe of points to file
+
+        Args:
+            filename (str | Path): filename of where to save points.
+        """
         file = Path(filename)
 
-        df = pd.DataFrame(self.points, columns=["x", "t"], dtype=float)
-        df.to_csv(file)
+        if self.df_points is None:
+            self.points_to_df()
+
+        self.df_points.to_csv(file)
