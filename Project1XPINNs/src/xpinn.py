dd14f407d9b1dcee1d0cebe92e540dbde2e0c23d
August Femtehjell <august.femtehjell@gmail.com>
2024-03-02 16:20:26 +0100

    Pulled PINN into own file

--- Project1XPINNs/src/xpinn.py
+++ Project1XPINNs/src/xpinn.py
@@ -1,21 +1,16 @@
 import jax.numpy as np
-from jax import jit, random, grad, vmap
 import json
+
+from jax import random
 from pathlib import Path
 from dataclasses import dataclass
-from base_network import init_network_params, neural_network
 from type_util import (
     Array,
     Activator,
-    Shape,
-    Callable,
-    GradientTransformation,
     Params,
     OptState,
-    Updates,
 )
-import optax
-from functools import partial
+from pinn import PINN
 
 
 @dataclass
@@ -29,123 +24,6 @@ class Interface:
 
     indices: list[int]
     points: Array
-
-
-class PINN:
-    def __init__(
-        self,
-        interior: Array,
-        boundary: Array,
-        activation: Activator,
-        rand_key: int = 0,
-    ) -> None:
-        """Physics-Informed Neural Network
-
-        Args:
-            interior (np.ndarray): Points internal to the subdomain
-            boundary (np.ndarray): Points along the boundary of the domain
-            activation (Activator): Activation function for the layers
-            rand_key (int): Initial key used to generate random values with jax
-        """
-        self.interior = interior
-        self.boundary = boundary
-        self.rand_key = rand_key
-        self.model = neural_network(activation)
-        self.v_model = jit(vmap(self.model, (None, 0)))
-
-        self.residual = lambda params, points: 0
-        self.v_residual = lambda params, points: 0
-
-        self.interior_loss = lambda params, args: 0
-        self.boundary_loss = lambda params, args: 0
-        self.interface_loss = lambda params, args: 0
-
-        self.loss = lambda params, args: 0
-        self.grad_loss = lambda params, args: 0
-
-    def init_params(self, sizes: Shape, optimizer: GradientTransformation) -> None:
-        """Initialize the parameters of the network
-
-        Args:
-            sizes (Shape): Shape of the layers, [in_size, hidden1, ..., hiddenN, out_size]
-            optimizer (GradientTransformation): Optax compatible optimizer
-        """
-        self.input_size = sizes[0]
-        key, self.rand_key = random.split(self.rand_key)
-        self.params = init_network_params(sizes, key)
-
-        self.optimizer = optimizer
-        self.optstate = optimizer.init(self.params)
-
-    @staticmethod
-    @partial(jit, static_argnums=(0, 3))
-    def update_iteration(
-        grad_loss: Callable[[Params, dict], Updates],
-        params: Params,
-        args: dict[str, Array],
-        optimizer: GradientTransformation,
-        optstate: OptState,
-    ) -> tuple[Params, OptState]:
-        """One iteration of updating the parameters
-
-        Args:
-            grad_loss (Callable[[Params, dict], Updates]): Gradient of the loss function
-            params (Params): Parameters of the neural network
-            args (dict[str, Array]): Arguments needed in the loss functions
-            optimizer (GradientTransformation): Chosen optax compatible optimizer
-            optstate (OptState): Current state of the optimizer
-
-        Returns:
-            tuple[Params, OptState]: Updated parameters and state of the optimizer
-        """
-        grads = grad_loss(params, args)
-        updates, optstate = optimizer.update(grads, optstate)
-        params = optax.apply_updates(params, updates)
-
-        return params, optstate
-
-    def create_loss(self) -> Callable[[Params, dict[str, Array]], float]:
-        """Creates the jitted loss function, after internal functions are set
-
-        Returns:
-            Callable[[Params, dict], float]: Loss function for the network
-        """
-
-        @jit
-        def loss(params: Params, args: dict[str, Array]) -> float:
-            """The loss function of the network
-
-            Args:
-                params (Params): Network parameters, i.e. weights and biases for the given layers
-                args (dict): Relevant points, e.g.
-                    {
-                        "Internal": [[1, 2], ...]
-                        "Boundary": [[0, 0], ...]
-                        "12Interface_points": [[3, 3], ...]
-                        "12Interface_values_here": [1, 2, ...]
-                        "12Interface_values_there": [1.2, 1.8, ...]
-                    }
-                    such that the internal functions can be
-
-                    def interface_loss_1(params, args):
-                        I_p = args["12Interface_points"]
-                        I_val_i = args["12Interface_values_here"]
-                        I_val_j = args["12Interace_values_here"]
-                        return l2_loss(I_val_i - I_val_j)
-
-            Returns:
-                float: Evaluated loss
-            """
-            return (
-                self.interior_loss(params, args)
-                + self.boundary_loss(params, args)
-                + self.interface_loss(params, args)
-            )
-
-        self.loss = loss
-        self.grad_loss = jit(grad(loss))
-
-        return loss
 
 
 class XPINN:
@@ -275,44 +153,3 @@ class XPINN:
                 )
 
         return np.asarray(losses)
-
-
-@jit
-def insert_elem(arr: Array, idx: int, jdx: int, elem) -> Array:
-    arr = arr.at[idx, jdx].set(elem)
-    return arr
-
-
-if __name__ == "__main__":
-    # base_dir = Path(__file__).parents[1]
-    # data_dir = base_dir / "data"
-
-    # network = XPINN(data_dir / "XPINN_template.json")
-
-    for i in range(10):
-        res = PINN.compute_add(i, i)
-        print(res)
-
-    a = np.arange(0, 10)
-    b = np.arange(4, 14)
-
-    print(PINN.compute_add(a, b))
-    A = PINN(np.zeros(3), np.zeros(3), np.tanh)
-    # loss = A.create_loss()
-    # for i in range(10):
-    #     print(loss(float(i)))
-
-    # grad_loss = grad(loss)
-    # for i in range(10):
-    #     print(grad_loss(float(i)))
-
-    x = np.arange(10)
-
-    # @jit
-    def foo():
-        # global x
-        return x * x
-
-    print(foo())
-    x = x.at[0].set(100)
-    print(foo())
