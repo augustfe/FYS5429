e86346c483b7ef45735e5a3bde3253b0e456bd09
August Femtehjell <august.femtehjell@gmail.com>
2024-03-04 19:24:31 +0100

    Documented code

--- Project1XPINNs/src/region/shapes.py
+++ Project1XPINNs/src/region/shapes.py
@@ -1,7 +1,6 @@
 from type_util import Array, Scalar
 import jax.numpy as np
 from jax import jit, vmap, lax
-import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
 
 
@@ -38,11 +37,11 @@ class ConvexPolygon(Shape):
             vertices (Array): The vertices of the polygon, in the order they appear
         """
         self.vertices = vertices
-        self.vectors = create_vectors(vertices)
+        self.vectors = _create_vectors(vertices)
 
     def is_inside(self, point: Array) -> bool:
         affine_points = point - self.vertices
-        return point_inside_poly(self.vectors, affine_points)
+        return _point_inside_poly(self.vectors, affine_points)
 
 
 class Circle(Shape):
@@ -51,35 +50,89 @@ class Circle(Shape):
         self.radius = radius
 
     def is_inside(self, point: Array) -> bool:
-        return point_inside_circ(self.center, point, self.radius)
+        return _point_inside_circ(self.center, point, self.radius)
 
 
+# We place the methods outside the classes, such
+# that they behave properly when jitted
+# ----------------- POLYGON METHODS ----------------- #
 @jit
-def create_vectors(points: Array) -> Array:
+def _create_vectors(points: Array) -> Array:
+    """Calculates the vector between $x_i$ and $x_{i+1}$
+
+    Args:
+        points (Array): Points to turn into vectors
+
+    Returns:
+        Array: Vectors, where index i is the vector between i and i+1
+    """
     shifted_points = np.roll(points, -1, axis=0)
     return shifted_points - points
 
 
 @jit
-def get_side(affine_segment: Array, affine_point: Array) -> bool:
-    return lax.select(np.cross(affine_segment, affine_point) < 0, 1, -1)
+def _point_inside_poly(affine_segments: Array, affine_points: Array) -> bool:
+    """Check whether a point is inside a polygon.
 
+    Args:
+        affine_segments (Array): Vectors between polygon vertices
+        affine_points (Array): Difference between the given point, and the polygon vertices
 
-@jit
-def point_inside_poly(affine_segments: Array, affine_points: Array) -> bool:
-    v_get_side = vmap(get_side)
+    Returns:
+        bool: True, if the point lies inside the shape
+    """
+    # Vectorize to check each segment-point pair
+    v_get_side = vmap(_get_side)
     side = v_get_side(affine_segments, affine_points)
+
+    # Roll and array_equal to check all values are the same (all left or all right)
     side_C = np.roll(side, 1)
     return np.array_equal(side, side_C)
 
 
 @jit
-def point_inside_circ(center: Array, point: Array, radius: Scalar) -> bool:
+def _get_side(affine_segment: Array, affine_point: Array) -> bool:
+    """Calculate which side of the line a point lies.
+
+    We use the cross product in order to check which side of the line we are on
+
+    lax.select(pred, on_true, on_false)
+    is equivalent to
+    if pred:
+        return on_true
+    else:
+        return on_false
+
+    Args:
+        affine_segment (Array): Vector segment
+        affine_point (Array): Point - origin
+
+    Returns:
+        bool: 1, if the point lies to the left of the line, -1 else.
+    """
+    return lax.select(np.cross(affine_segment, affine_point) < 0, 1, -1)
+
+
+# ----------------- CIRCLE METHODS ----------------- #
+@jit
+def _point_inside_circ(center: Array, point: Array, radius: Scalar) -> bool:
+    """Check if the point is inside the circle
+
+    Args:
+        center (Array): Center of the circle
+        point (Array): Point to check
+        radius (Scalar): Radius of the circle
+
+    Returns:
+        bool: True, if the point is contained in the circle
+    """
     dist = np.sum((center - point) ** 2)
     return dist < radius
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     vertices = np.asarray([[1, 1], [2, 1], [2, 2], [1, 2]])
     square = ConvexPolygon(vertices)
     circ = Circle(np.asarray([1.5, 1.5]), 1)
