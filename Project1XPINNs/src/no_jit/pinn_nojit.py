31bf5afe7e95d3b7b1766a6ba461d378154287e1
Junmiao <junmiao.hu1@gmail.com>
2024-03-16 00:40:38 +0100

    Added no jit versions of the PINN models for testing

---
+++ Project1XPINNs/src/no_jit/pinn_nojit.py
@@ -0,0 +1,142 @@
+import optax
+import jax.numpy as np
+from base_network import init_network_params, neural_network
+from jax import grad, random, vmap
+from type_util import (
+    Activator,
+    Array,
+    Callable,
+    GradientTransformation,
+    Shape,
+    Params,
+    Updates,
+    OptState,
+)
+from functools import partial
+
+
+class PINN:
+    def __init__(
+        self,
+        interior: Array,
+        boundary: Array,
+        activation: Activator,
+        rand_key: int = 0,
+    ) -> None:
+        """Physics-Informed Neural Network
+
+        Args:
+            interior (np.ndarray): Points internal to the subdomain
+            boundary (np.ndarray): Points along the boundary of the domain
+            activation (Activator): Activation function for the layers
+            rand_key (int): Initial key used to generate random values with jax
+        """
+        self.interior = interior
+        self.boundary = boundary
+        self.rand_key = rand_key
+        self.model = neural_network(activation)
+        self.v_model = vmap(self.model, (None, 0))
+
+        self.residual = lambda params, points: 0
+        self.v_residual = lambda params, points: 0
+
+        self.interior_loss = lambda params, args: 0
+        self.boundary_loss = lambda params, args: 0
+        self.interface_loss = lambda params, args: 0
+
+        self.loss = lambda params, args: 0
+        self.grad_loss = lambda params, args: 0
+
+    def init_params(self, sizes: Shape, optimizer: GradientTransformation) -> None:
+        """Initialize the parameters of the network
+
+        Args:
+            sizes (Shape): Shape of the layers, [in_size, hidden1, ..., hiddenN, out_size]
+            optimizer (GradientTransformation): Optax compatible optimizer
+        """
+        self.input_size = sizes[0]
+        self.output_size = sizes[-1]
+        key, self.rand_key = random.split(self.rand_key)
+        self.params = init_network_params(sizes, key)
+
+        self.optimizer = optimizer
+        self.optstate = optimizer.init(self.params)
+
+    @staticmethod
+    def update_iteration(
+        grad_loss: Callable[[Params, dict], Updates],
+        params: Params,
+        args: dict[str, Array],
+        optimizer: GradientTransformation,
+        optstate: OptState,
+    ) -> tuple[Params, OptState]:
+        """One iteration of updating the parameters
+
+        Args:
+            grad_loss (Callable[[Params, dict], Updates]): Gradient of the loss function
+            params (Params): Parameters of the neural network
+            args (dict[str, Array]): Arguments needed in the loss functions
+            optimizer (GradientTransformation): Chosen optax compatible optimizer
+            optstate (OptState): Current state of the optimizer
+
+        Returns:
+            tuple[Params, OptState]: Updated parameters and state of the optimizer
+        """
+        grads = grad_loss(params, args)
+        updates, optstate = optimizer.update(grads, optstate)
+        params = optax.apply_updates(params, updates)
+
+        return params, optstate
+
+    def create_loss(self) -> Callable[[Params, dict[str, Array]], float]:
+        """Creates the jitted loss function, after internal functions are set
+
+        Returns:
+            Callable[[Params, dict], float]: Loss function for the network
+        """
+
+        def loss(params: Params, args: dict[str, Array]) -> float:
+            """The loss function of the network
+
+            Args:
+                params (Params): Network parameters, i.e. weights and biases for the given layers
+                args (dict): Relevant points, e.g.
+                    {
+                        "Internal": [[1, 2], ...]
+                        "Boundary": [[0, 0], ...]
+                        "12Interface_points": [[3, 3], ...]
+                        "12Interface_values_here": [1, 2, ...]
+                        "12Interface_values_there": [1.2, 1.8, ...]
+                    }
+                    such that the internal functions can be
+
+                    def interface_loss_1(params, args):
+                        I_p = args["12Interface_points"]
+                        I_val_i = args["12Interface_values_here"]
+                        I_val_j = args["12Interace_values_here"]
+                        return l2_loss(I_val_i - I_val_j)
+
+            Returns:
+                float: Evaluated loss
+            """
+            return (
+                self.interior_loss(params, args)
+                + self.boundary_loss(params, args)
+                + self.interface_loss(params, args)
+            )
+
+        self.loss = loss
+        self.grad_loss = grad(loss)
+
+        return loss
+
+    def predict(self, args: dict[str, Array]):
+        b = args["boundary"]
+        i = args["interior"]
+        if b.size == 0:
+            points = i
+        else:
+            points = np.vstack([b, i])
+
+        prediction = self.v_model(self.params, points)
+        return points, prediction
---
+++ Project1XPINNs/src/no_jit/xpinn_nojit.py
@@ -0,0 +1,204 @@
+import jax.numpy as np
+import json
+
+from jax import random
+from pathlib import Path
+from dataclasses import dataclass
+from type_util import Array, Activator, Params, OptState, Shape
+from no_jit.pinn_nojit import PINN
+import numpy as onp
+
+
+@dataclass
+class Interface:
+    """Dataclass for storing interface information
+
+    args:
+        indices (list[int]): indicies of corresponding PINNs in the XPINN class
+        points (np.ndarray): points along the interface
+    """
+
+    indices: list[int]
+    points: Array
+
+
+class XPINN:
+    def __init__(
+        self,
+        input_file: str | Path,
+        activation: Activator,
+        seed: int = 0,
+    ) -> None:
+        """Extended Physics-Informed Neural Network
+
+        Args:
+            input_file (str | Path): JSON-file containing the information about the domain/subdomain points
+            activation (Activator): Activation function for the PINNs
+            seed (int, optional): Seed to use for random values. Defaults to 0.
+
+        Raises:
+            FileExistsError: If the given file does not exist
+        """
+        input_file = Path(input_file)
+
+        if not input_file.exists():
+            raise FileExistsError(f"The file {input_file} does not exist.")
+
+        with open(input_file, "r") as infile:
+            data = json.load(infile)
+
+        # Set initial jax random key
+        key = random.PRNGKey(seed)
+
+        self.PINNs: list[PINN] = []
+        self.Interfaces: list[Interface] = []
+        self.main_args = {}
+
+        # Store data for each PINN
+        for i, item in enumerate(data["XPINNs"]):
+            print(f"PINN number {i} initializing")
+            interior = np.asarray(item["Internal points"])
+            boundary = np.asarray(item["Boundary points"])
+
+            self.main_args[i] = {"boundary": boundary, "interior": interior}
+
+            key, subkey = random.split(key)
+            new_PINN = PINN(interior, boundary, activation, key)
+            self.PINNs.append(new_PINN)
+
+            key = subkey
+
+        # Store data for each Interface
+        for item in data["Interfaces"]:
+            indices = item["XPINNs"]
+            points = np.asarray(item["Points"])
+            new_Interface = Interface(indices, points)
+            self.Interfaces.append(new_Interface)
+
+        for interface in self.Interfaces:
+            a, b = sorted(interface.indices)
+            for idx in interface.indices:
+                args = self.main_args[idx]
+                args[f"interface {a}{b}"] = interface.points
+
+    def set_loss(self) -> None:
+        """Initialize the loss functions for each PINN."""
+        for pinn in self.PINNs:
+            pinn.create_loss()
+
+    def initialize_params(self, sizes: list[Shape], optimizer) -> None:
+        """Initialize the parameters for each PINN.
+
+        Args:
+            sizes (list[int]): Sizes of the layers for each PINN
+            optimizer ([type]): Optax compatible optimizer
+        """
+        self.input_size = sizes[0][0]
+        self.output_size = sizes[0][-1]
+
+        for pinn, size in zip(self.PINNs, sizes):
+            pinn.init_params(size, optimizer)
+
+    def transfer_interface_values(self) -> None:
+        """Communicate relevant values across each interface."""
+        for interface in self.Interfaces:
+            i, j = interface.indices
+            self._interface_comm(i, j)
+            self._interface_comm(j, i)
+
+    def _interface_comm(self, i: int, j: int) -> None:
+        """Compute values across boundary, and transfer.
+
+        Args:
+            i (int): Index of PINN to compute values for
+            j (int): Index of PINN to save values for
+        """
+        a, b = sorted([i, j])
+        pinn_i = self.PINNs[i]
+        args_i = self.main_args[i]
+        args_j = self.main_args[j]
+
+        points = args_i[f"interface {a}{b}"]
+        res_i = pinn_i.v_residual(pinn_i.params, points)
+        args_j[f"interface res {i}"] = res_i
+
+        val_i = pinn_i.v_model(pinn_i.params, points)
+        args_j[f"interface val {i}"] = val_i
+
+    def optimize_iter(self, epoch: int) -> None:
+        """One iteration of optimizing the networks.
+
+        Returns:
+            list[int]: Losses for the different networks
+        """
+        self.transfer_interface_values()
+
+        for i, pinn in enumerate(self.PINNs):
+            args = self.main_args[i]
+
+            iter_loss = pinn.loss(pinn.params, args)
+            #print(pinn.interface_loss(pinn.params, args))
+
+            self.losses[i, epoch] = iter_loss
+
+            params, optstate = pinn.update_iteration(
+                pinn.grad_loss,
+                pinn.params,
+                args,
+                pinn.optimizer,
+                pinn.optstate,
+            )
+            # Typehinting
+            params: Params
+            optstate: OptState
+
+            pinn.params, pinn.optstate = params, optstate
+
+    def run_iters(self, num_epoch: int) -> Array:
+        self.losses = onp.zeros((len(self.PINNs), num_epoch))
+
+        print_num = max(num_epoch // 10, 1)
+
+        self.set_loss()
+
+        for epoch in range(num_epoch):
+            self.optimize_iter(epoch)
+
+            iter_loss = sum(self.losses[:, epoch])
+
+            if epoch % print_num == 0:
+                print(
+                    f"{epoch / num_epoch * 100:.2f}% iter = {epoch} of {num_epoch}: Total loss = {iter_loss}"
+                )
+
+        print(
+            f"{(epoch+1) / num_epoch * 100:.2f}% iter = {epoch + 1} of {num_epoch}: Total loss = {iter_loss}"
+        )
+
+        return np.asarray(self.losses)
+
+    def predict(self, input_file: str | Path = None):
+        if input_file:
+            main_args = {}
+            with open(input_file) as infile:
+                data = json.load(infile)
+
+            for i, item in enumerate(data["XPINNs"]):
+                interior = np.asarray(item["Internal points"])
+                boundary = np.asarray(item["Boundary points"])
+
+                main_args[i] = {"boundary": boundary, "interior": interior}
+
+        else:
+            main_args = self.main_args
+
+        total_points = []
+        predictions = []
+
+        for i, pinn in enumerate(self.PINNs):
+            points, prediction = pinn.predict(main_args[i])
+
+            total_points.append(points)
+            predictions.append(prediction)
+
+        return total_points, predictions
