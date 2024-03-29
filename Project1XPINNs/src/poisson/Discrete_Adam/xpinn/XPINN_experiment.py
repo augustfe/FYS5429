a262798fe988b68d96caa4cb5cd1d98ecbe9cdfa
Junmiao <junmiao.hu1@gmail.com>
2024-03-29 12:21:01 +0100

    Added discrete Adam - PINN has unknown error

--- Project1XPINNs/src/poisson/Discrete_Adam/xpinn/XPINN_experiment.py
+++ Project1XPINNs/src/poisson/Discrete_Adam/xpinn/XPINN_experiment.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 from utils import data_path
 from base_network import neural_network
 from poisson.poisson_utils import boundary_loss_factory, interior_loss_factory, crude_rel_L2, interface_loss_factory
-### Not done
 if __name__ == '__main__':
     ### Set files
     name = str(sys.argv[1])
@@ -22,26 +21,11 @@ if __name__ == '__main__':
     model = neural_network(activation)
     v_model = vmap(model, (None, 0))
 
-    ### Set RHS
-    def eval_rhs(x):
-        a = 0
-        if 0.25<=x[0]<=0.75 and 0.25<=x[1]<=0.75:
-            a=1
-        return a
-
-    def f():
-        points=xpinn.PINNs[0].interior
-        f=onp.zeros_like(points)
-        for i,point in enumerate(points):
-            f[i] = eval_rhs(point)
-        return np.array(f)
-
-    rhs = f()
     # Set losses
     p0, p1 = xpinn.PINNs
     p0.boundary_loss = boundary_loss_factory(p0,0.0)
-    p0.interior_loss = interior_loss_factory(p0, 0)  # implementing rhs
-    p1.interior_loss = interior_loss_factory(p1, rhs)
+    p0.interior_loss = interior_loss_factory(p0, 0.0)  # implementing rhs
+    p1.interior_loss = interior_loss_factory(p1, 1.0)
     p0.interface_loss = interface_loss_factory(xpinn,0, 1)
     p1.interface_loss = interface_loss_factory(xpinn,1, 0)
 
@@ -61,29 +45,33 @@ if __name__ == '__main__':
     ### Setting iterations
     n_iter = 200000
     xpinn.set_loss()
-    nr_saved_points = 400
+    nr_saved_points = 100
     ### prepping relative errors
     ##Getting initial predictions
     points, predictions = xpinn.predict(file_test)
     total_points = np.concatenate(points)
     total_pred = np.concatenate(predictions)
-
+    sorted_indices = np.lexsort((total_points[:,1],total_points[:,0]))
+    sorted_pred = total_pred[sorted_indices]
+    
     # Getting true value
-    u_vmap = vmap(u_star, (0))
-    true_value = u_vmap(total_points).reshape(-1, 1)
-
+    file = f"../true_solution.npz"
+    with onp.load(file) as true_file:
+        true_sol = true_file["arr_0"]
+        
+    true_integral = np.sqrt(np.sum(true_sol**2))
     #Setting the L2 relative errors
     l2_errors = onp.zeros(nr_saved_points + 1)
-    l2_errors[0] = crude_rel_L2(total_pred, true_value)
-    
+    print("calculating crude_rel_L2")
+    l2_errors[0] = crude_rel_L2(sorted_pred, true_sol, true_integral)
+    print(l2_errors[0])
     for i in tqdm(range(nr_saved_points)):
-        ### Run iterations
+       ### Run iterations
         losses = xpinn.run_iters(int(n_iter/nr_saved_points))
-        
-        ### Calculate rel l2 error
+       
+       ### Calculate rel l2 error
         _, predictions = xpinn.predict(file_test)
-        total_pred = np.concatenate(predictions)
-        l2_errors[i+1] = crude_rel_L2(total_pred, true_value)
-
+        sorted_pred = np.concatenate(predictions)[sorted_indices]
+        l2_errors[i+1] = crude_rel_L2(sorted_pred, true_sol, true_integral)
     print(l2_errors)
     onp.savez(f"results/L2_errors_{name}", l2_errors)
